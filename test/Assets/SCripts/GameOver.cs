72c92930b633e30a31f26ce90d48606f1d043110
EgilsDev <egils.smidris@gmail.com>
2023-04-02 23:37:06 +0300

    Added Exit and restart buttons

--- test/Assets/SCripts/GameOver.cs
+++ test/Assets/SCripts/GameOver.cs
@@ -7,10 +7,17 @@ public class GameOver : MonoBehaviour
 {
     public string textvalue;
     public Text textelement;
+
+    public Button exit_button;
+    public Button restart_button;
+
     // Start is called before the first frame update
     void Start()
     {
         textelement.text = "";
+
+        exit_button.gameObject.SetActive(false);
+        restart_button.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,6 +29,12 @@ public class GameOver : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "Ball"){
             textelement.text = textvalue;
+
+            exit_button.gameObject.SetActive(true);
+            restart_button.gameObject.SetActive(true);
+
         }
+
+  
     }
 }
